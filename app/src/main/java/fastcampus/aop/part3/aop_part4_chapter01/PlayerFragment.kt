22e0a1e254f6fc0350a03d99481137f3608121b7
egchoi <egchoi.dev@gmail.com>
2022-04-18 22:33:05 +0900

    exo player

--- app/src/main/java/fastcampus/aop/part3/aop_part4_chapter01/PlayerFragment.kt
+++ app/src/main/java/fastcampus/aop/part3/aop_part4_chapter01/PlayerFragment.kt
@@ -1,11 +1,17 @@
 package fastcampus.aop.part3.aop_part4_chapter01
 
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import androidx.constraintlayout.motion.widget.MotionLayout
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.exoplayer2.MediaItem
+import com.google.android.exoplayer2.Player
+import com.google.android.exoplayer2.SimpleExoPlayer
+import com.google.android.exoplayer2.source.ProgressiveMediaSource
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
 import fastcampus.aop.part3.aop_part4_chapter01.adapter.VideoAdapter
 import fastcampus.aop.part3.aop_part4_chapter01.databinding.FragmentPlayerBinding
 import fastcampus.aop.part3.aop_part4_chapter01.dto.VideoDto
@@ -23,6 +29,8 @@ class PlayerFragment : Fragment(R.layout.fragment_player) {
 
     private lateinit var videoAdapter: VideoAdapter
 
+    private var player: SimpleExoPlayer? = null
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val fragmentPlayerBinding = FragmentPlayerBinding.bind(view)
@@ -32,9 +40,10 @@ class PlayerFragment : Fragment(R.layout.fragment_player) {
 
         initMotionLayoutEvent(fragmentPlayerBinding)
         initRecyclerView(fragmentPlayerBinding)
+        initPlayer(fragmentPlayerBinding)
+        initControlButton(fragmentPlayerBinding)
 
     }
-
 
     private fun initMotionLayoutEvent(fragmentPlayerBinding: FragmentPlayerBinding) {
         fragmentPlayerBinding.playerMotionLayout.setTransitionListener(object :
@@ -106,17 +115,67 @@ class PlayerFragment : Fragment(R.layout.fragment_player) {
         }
     }
 
+    private fun initPlayer(fragmentPlayerBinding: FragmentPlayerBinding) {
+        context?.let {
+            player = SimpleExoPlayer.Builder(it).build()
+        }
+
+        fragmentPlayerBinding.playerView.player = player
+
+        binding?.let {
+            player?.addListener(object : Player.EventListener {
+                override fun onIsPlayingChanged(isPlaying: Boolean) {
+                    super.onIsPlayingChanged(isPlaying)
+                    if (isPlaying) {
+                        // todo : bottomPlayerControlButton is not visible
+                        it.bottomPlayerControlButton.setImageResource(R.drawable.ic_baseline_pause_24)
+                    } else {
+                        it.bottomPlayerControlButton.setImageResource(R.drawable.ic_baseline_play_arrow_24)
+                    }
+                }
+            })
+        }
+    }
+
+    private fun initControlButton(fragmentPlayerBinding: FragmentPlayerBinding) {
+        fragmentPlayerBinding.bottomPlayerControlButton.setOnClickListener {
+            val player = this.player ?: return@setOnClickListener
+
+            if (player.isPlaying) {
+                player.pause()
+            } else {
+                player.play()
+            }
+        }
+    }
+
     fun play(url: String, title: String) {
+
+        context?.let {
+            val dataSourceFactory = DefaultDataSourceFactory(it)
+            val mediaSource = ProgressiveMediaSource.Factory(dataSourceFactory)
+                .createMediaSource(MediaItem.fromUri(Uri.parse(url)))
+            player?.setMediaSource(mediaSource)
+            player?.prepare()
+            player?.play()
+        }
+
         binding?.let {
             it.playerMotionLayout.transitionToEnd()
             it.bottomTitleTextView.text = title
         }
     }
 
+    override fun onStop() {
+        super.onStop()
+        player?.pause()
+    }
+
 
     override fun onDestroy() {
         super.onDestroy()
         binding = null;
+        player?.release()
     }
 
 }
