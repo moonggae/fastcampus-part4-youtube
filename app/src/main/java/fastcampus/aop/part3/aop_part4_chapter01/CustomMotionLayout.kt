95cea557764e382c066820a86ab12ac6e7bdda1a
egchoi <egchoi.dev@gmail.com>
2022-04-18 21:26:25 +0900

    main recyclerview scroll event

---
+++ app/src/main/java/fastcampus/aop/part3/aop_part4_chapter01/CustomMotionLayout.kt
@@ -0,0 +1,81 @@
+package fastcampus.aop.part3.aop_part4_chapter01
+
+import android.content.Context
+import android.graphics.Rect
+import android.util.AttributeSet
+import android.view.GestureDetector
+import android.view.MotionEvent
+import android.view.View
+import androidx.constraintlayout.motion.widget.MotionLayout
+
+class CustomMotionLayout(context : Context, attrs : AttributeSet? = null) : MotionLayout(context, attrs) {
+    private var motionTouchStarted = false
+    private val mainContainerLayout by lazy {
+        findViewById<View>(R.id.mainContainerLayout)
+    }
+    private val hitRect = Rect()
+
+    init {
+        setTransitionListener(object : TransitionListener{
+            override fun onTransitionStarted(
+                motionLayout: MotionLayout?,
+                startId: Int,
+                endId: Int
+            ) { }
+            override fun onTransitionChange(
+                motionLayout: MotionLayout?,
+                startId: Int,
+                endId: Int,
+                progress: Float
+            ) { }
+            override fun onTransitionCompleted(motionLayout: MotionLayout?, currentId: Int) {
+                motionTouchStarted = false
+            }
+            override fun onTransitionTrigger(
+                motionLayout: MotionLayout?,
+                triggerId: Int,
+                positive: Boolean,
+                progress: Float
+            ) { }
+        })
+    }
+
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+
+        when(event.actionMasked){
+            MotionEvent.ACTION_UP, MotionEvent.ACTION_CANCEL -> {
+                motionTouchStarted = false
+                return super.onTouchEvent(event)
+            }
+        }
+
+        if(motionTouchStarted.not()){
+            mainContainerLayout.getHitRect(hitRect)
+            motionTouchStarted = hitRect.contains(event.x.toInt(), event.y.toInt())
+        }
+
+        return super.onTouchEvent(event) && motionTouchStarted
+    }
+
+    private val gestureListener by lazy {
+        object : GestureDetector.SimpleOnGestureListener() {
+            override fun onScroll(
+                e1: MotionEvent,
+                e2: MotionEvent,
+                distanceX: Float,
+                distanceY: Float
+            ): Boolean {
+                mainContainerLayout.getHitRect(hitRect)
+                return hitRect.contains(e1.x.toInt(), e1.y.toInt())
+            }
+        }
+    }
+
+    private val gestureDetector by lazy {
+        GestureDetector(context, gestureListener)
+    }
+
+    override fun onInterceptTouchEvent(event: MotionEvent?): Boolean {
+        return gestureDetector.onTouchEvent(event)
+    }
+}
